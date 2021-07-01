9287fd1c4677037ddc1db389e7567b576e0691b4
psjw <dlrudtn108@gmail.com>
2021-07-01 21:11:05 +0900

    change2

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql");
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'mysql',
-    user: 'root',
-    password: 'lks0108!',
-    database: 'myapp'
-});
-exports.pool = pool;
